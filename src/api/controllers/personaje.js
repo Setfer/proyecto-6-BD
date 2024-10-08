4cd45ef78ed0cbad637ce7031e9c4fb00c17eefd
Setfer <andres88lacarolina@gmail.com>
2024-10-08 16:18:17 +0200

    Update personaje.js

--- src/api/controllers/personaje.js
+++ src/api/controllers/personaje.js
@@ -32,46 +32,51 @@ const postPersonaje = async (req, res, next) => {
 
 const updatePersonaje = async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const oldPersonaje = await Personaje.findById(id);
+    const { id } = req.params
+    const oldPersonaje = await Personaje.findById(id)
 
     if (!oldPersonaje) {
-      return res.status(404).json('Personaje no encontrado');
+      return res.status(404).json('Personaje no encontrado')
     }
 
     if (req.body.juego) {
-      const oldJuegos = oldPersonaje.juego || [];
-      const newJuegos = Array.isArray(req.body.juego) ? req.body.juego : [req.body.juego];
+      const oldJuegos = oldPersonaje.juego || []
+      const newJuegos = Array.isArray(req.body.juego)
+        ? req.body.juego
+        : [req.body.juego]
 
-      const juegosDuplicados = newJuegos.filter(juego => oldJuegos.includes(juego));
+      const juegosDuplicados = newJuegos.filter((juego) =>
+        oldJuegos.includes(juego)
+      )
 
       if (juegosDuplicados.length > 0) {
-        return res.status(400).json(`El juego ya está incluido: ${juegosDuplicados.join(', ')}`);
+        return res
+          .status(400)
+          .json(`El juego ya está incluido: ${juegosDuplicados.join(', ')}`)
       }
 
-      const JuegosActualizados = [...new Set([...oldJuegos, ...newJuegos])];
-
+      const JuegosActualizados = [...new Set([...oldJuegos, ...newJuegos])]
+      req.body.juego = JuegosActualizados
       const up = await Personaje.findByIdAndUpdate(
         id,
-        { juego: JuegosActualizados },
+        { $set: req.body },
         { new: true }
-      );
+      )
 
-      return res.status(200).json(up);
+      return res.status(200).json(up)
     }
-    
+
     const update = await Personaje.findByIdAndUpdate(
       id,
       { $set: req.body },
       { new: true }
-    );
+    )
 
-    return res.status(200).json(update);
+    return res.status(200).json(update)
   } catch (error) {
-    return res.status(500).json('Algo ha fallado');
+    return res.status(500).json('Algo ha fallado')
   }
-};
-
+}
 
 const deletePersonaje = async (req, res, next) => {
   try {
